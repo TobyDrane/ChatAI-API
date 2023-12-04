a942eccb27bc0e0b9126be675d50a254bda77f80
Toby Drane <tdrane@no10.gov.uk>
2023-12-04 21:23:39 +0000

    Better API documentation

--- api/controller/conversation.py
+++ api/controller/conversation.py
@@ -27,12 +27,31 @@ conversation_router = APIRouter(
 
 @conversation_router.get("")
 async def get_conversations(request: Request) -> list[Conversation]:
+    """
+    ## List all conversations
+
+    Use this endpoint to retrieve a list of all current conversation with the database.
+    This also includes all the messages in each conversation and for large amounts of data
+    may be slow.
+
+    ### Click  `Try it out` to use the endpoint
+
+    """  # noqa: E501
     db = get_mongo_db_from_request(request)
     return get_all_conversations(db)
 
 
 @conversation_router.post("")
 async def create_conversation(request: Request) -> Conversation:
+    """
+    ## Create new conversation
+
+    Use this endpoint to create a new blank conversation within the database. Once a conversation
+    is created the user can chat to the AI for the given conversation.
+
+    ### Click  `Try it out` to use the endpoint
+
+    """  # noqa: E501
     db = get_mongo_db_from_request(request)
     return insert_conversation(db)
 
@@ -43,6 +62,28 @@ async def create_conversation_completion(
     conversation_completion_request: ConversationCompletionRequest,
     conversation_id: str,
 ):
+    """
+    ## Conversation chat completion
+
+    Using this endpoint you can chat to the AI for a given conversation. The AI will respond with
+    a message based on the users input. All messages via this endpoint are stored within the database.
+
+    ### Inputs
+
+    | Parameters                        | Usage               | Example values   | Definition                                      |
+    |-----------------------------------|---------------------|------------------|-------------------------------------------------|
+    | `conversation_id`                 | URL Parameter       |                  | The conversation ID string                      |
+    | `conversation_completion_request` | JSON Request Body   | See below        | The chat completion request model               |
+
+    ```json
+    {
+            "message": "How would I create a chocolate cake?",
+            "stream": "true",
+    }
+    ```
+
+    ### Click  `Try it out` to use the endpoint
+    """  # noqa: E501
     db = get_mongo_db_from_request(request)
 
     if not check_conversation_exists(db, conversation_id):
